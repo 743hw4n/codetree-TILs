e2cc18337a534db9d55f40b6b783491637388285
taehwan <80195853+taehwan011@users.noreply.github.com>
2024-07-17 22:52:55 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double a = 5.26;
+    double b = 8.27;
+
+    double c = a * b;
+
+
+    cout << fixed;
+    cout.precision(3);
+
+    cout << c;
+
+    return 0;
+}
