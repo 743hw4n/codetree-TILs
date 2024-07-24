b1e53b1f0609763aff9a564c1352708e0b66f7c8
taehwan <80195853+taehwan011@users.noreply.github.com>
2024-07-24 22:23:03 +0900

    Title: 황금비율 토스트, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240724/황금비율 토스트/golden-toast.cpp
+++ 240724/황금비율 토스트/golden-toast.cpp
@@ -2,49 +2,53 @@
 #include <list>
 using namespace std;
 
+
+
 int main() {
-    list<char> l;
+	list<char> l;
 
-    int n, m;
-    cin >> n >> m;
+	int n, m;
+	cin >> n >> m;
 
-    while (n--) {
-        char c;
-        cin >> c;
-        l.push_back(c);
-    }
+	while (n--) {
+		char c;
+		cin >> c;
+		l.push_back(c);
+	}
 
-    list<char>::iterator it = l.end();
+	list<char>::iterator it = l.end();
 
-    while (m--) {
-        char cmd;
-        cin >> cmd;
+	while (m--) {
+		char cmd;
+		cin >> cmd;
 
-        if (cmd == 'L') {
-            if (it != l.begin()) {
-                --it;
-            }
-        }
-        else if (cmd == 'R') {
-            if (it != l.end()) {
-                ++it;
-            }
-        }
-        else if (cmd == 'D') {
-            if (it != l.end()) {
-                it = l.erase(it);
-            }
-        }
-        else if (cmd == 'P') {
-            char x;
-            cin >> x;
-            l.insert(it, x);
-        }
-    }
+		if (cmd == 'L')
+		{
+			if (it == l.begin()) return;
+			it--;
+		}
 
-    for (it = l.begin(); it != l.end(); ++it) {
-        cout << *it;
-    }
+		else if (cmd == 'R')
+		{
+			if (it == l.end()) return;
+			it++;
+		}
 
-    return 0;
+		else if (cmd == 'D') {
+			if (it == l.end()) return;
+			l.erase(it);
+		}
+
+		else if (cmd == 'P') {
+			char x;
+			cin >> x;
+			l.insert(it, x);
+		}
+	}
+
+	for (it = l.begin(); it != l.end(); it++) {
+		cout << *it;
+	}
+
+	return 0;
 }
