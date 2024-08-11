9a72fac3f19ca2f28b9eddb8b07e7a134276e5be
taehwan <80195853+taehwan011@users.noreply.github.com>
2024-08-11 18:46:27 +0900

    Title: 2021년 날짜의 유무, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240811/2021년 날짜의 유무/with-or-without-2021.cpp
+++ 240811/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -6,6 +6,8 @@ int month[12] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
 int day[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 bool isDay(int m, int d) {
+	if (m < 1 || m > 12) return false;
+
 	if (day[m - 1] > d)
 		return true;
 	else
