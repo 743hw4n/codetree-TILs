c484a57b76f52e122b688b3b400b592eaecc6ad1
taehwan <80195853+taehwan011@users.noreply.github.com>
2024-08-11 17:45:19 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240811/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 240811/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -1,49 +1,54 @@
 #include <iostream>
 using namespace std;
 
-bool isPart(int index, int* arr1, int* arr2, int b) {
-	for (int i = 0; i < b; i++) {
-		if (arr1[index] != arr2[i])
-			return false;
-		else
-			index++;
-	}
+bool isPart(int index, int* arr1, int* arr2, int a, int b) {
+    if (index + b > a) // 배열 범위 초과 검사
+        return false;
 
-	return true;
+    for (int i = 0; i < b; i++) {
+        if (arr1[index + i] != arr2[i])
+            return false;
+    }
+    return true;
 }
+
 int main() {
-	int a, b;
-	cin >> a >> b;
+    int a, b;
+    cin >> a >> b;
 
-	int* arr1 = new int[a];
-	int* arr2 = new int[b];
+    if (a < b) {
+        cout << "No" << endl;
+        return 0;
+    }
 
-	for (int i = 0; i < a; i++) {
-		int num;
-		cin >> num;
-		arr1[i] = num;
-	}
+    int* arr1 = new int[a];
+    int* arr2 = new int[b];
 
-	for (int i = 0; i < b; i++) {
-		int num;
-		cin >> num;
-		arr2[i] = num;
-	}
+    for (int i = 0; i < a; i++) {
+        cin >> arr1[i];
+    }
 
-	int index = 0;
+    for (int i = 0; i < b; i++) {
+        cin >> arr2[i];
+    }
 
-	for (int i = 0; i < a; i++) {
-		if (arr1[i] == arr2[0])
-		{
-			index = i;
-			break;
-		}
-	}
+    bool found = false;
+    for (int i = 0; i <= a - b; i++) { // 모든 가능한 위치에서 부분 수열 검사
+        if (arr1[i] == arr2[0]) {
+            if (isPart(i, arr1, arr2, a, b)) {
+                found = true;
+                break;
+            }
+        }
+    }
 
-	if (isPart(index, arr1, arr2, b))
-		cout << "Yes" << endl;
-	else
-		cout << "No" << endl;
+    if (found)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
 
-	return 0;
+    delete[] arr1; // 메모리 해제
+    delete[] arr2;
+
+    return 0;
 }
