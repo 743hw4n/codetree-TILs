05ada3c36c976369d9e55ffa5d4afdc02ff24a99
taehwan <80195853+taehwan011@users.noreply.github.com>
2024-08-11 18:42:04 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240811/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int month[12] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+int day[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+bool isDay(int m, int d) {
+	if (day[m - 1] > d)
+		return true;
+	else
+		return false;
+}
+
+int main() {
+	
+
+	int m, d;
+	cin >> m >> d;
+
+	if (isDay(m, d)) cout << "Yes";
+	else cout << "No";
+
+	return
