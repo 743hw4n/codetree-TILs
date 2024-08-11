1d649375436c30cf2f7bc52344083f837841390a
taehwan <80195853+taehwan011@users.noreply.github.com>
2024-08-11 17:39:44 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240811/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+bool isPart(int index, int* arr1, int* arr2, int b) {
+	for (int i = 0; i < b; i++) {
+		if (arr1[index] != arr2[i])
+			return false;
+		else
+			index++;
+	}
+
+	return true;
+}
+int main() {
+	int a, b;
+	cin >> a >> b;
+
+	int* arr1 = new int[a];
+	int* arr2 = new int[b];
+
+	for (int i = 0; i < a; i++) {
+		int num;
+		cin >> num;
+		arr1[i] = num;
+	}
+
+	for (int i = 0; i < b; i++) {
+		int num;
+		cin >> num;
+		arr2[i] = num;
+	}
+
+	int index = 0;
+
+	for (int i = 0; i < a; i++) {
+		if (arr1[i] == arr2[0])
+			index = i;
+	}
+
+	if (isPart(index, arr1, arr2, b))
+		cout << "Yes" << endl;
+	else
+		cout << "No" << endl;
+
+	return 0;
+}
