3736f7ab747274e4e3d9326706dc02265d5ad904
t_t_hwan <80195853+t-t-hwan@users.noreply.github.com>
2024-08-18 14:09:11 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240818/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int arr[100];
+int result = 0;
+
+int x1(int num1, int num2) { //최소공배수 return.
+	int small = min(num1, num2);
+	int x = 0; //최대공약수
+	for (int i = 1; i <= small; i++) {
+		if (num1 % i == 0 && num2 % i == 0)
+			x = i;
+	}
+	return x;
+}
+
+int x2(int num1, int num2) {
+	return num1 * num2 / x1(num1, num2);
+}
+
+int v(int n, int* arr) {
+	if (n == 1) return arr[0];
+	
+	int crr = x2(arr[n - 1], arr[n - 2]);
+
+	int rest = v(n - 1, arr);
+
+	return x2(crr, rest);
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	
+	v(n, arr);
+	cout << result;
+	return 0;
+}
