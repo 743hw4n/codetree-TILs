fa6db2ad7eca507d814d18177ad4d92e6d4164b9
ta2hwan <80195853+ta2hwan@users.noreply.github.com>
2024-08-31 12:29:09 +0900

    Title: 개인정보, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240831/개인정보/personal-info.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Student {
+public:
+	string name;
+	int height;
+	double weight;
+
+	Student() {};
+	Student(string name, int height, double weight){
+		this->name = name;
+		this->height = height;
+		this->weight = weight;
+	}
+
+	void print() {
+		cout  << name << " " << height << " " << weight << '\n';
+	}
+};
+
+bool cmp1(Student a, Student b) {
+	return a.name < b.name;
+}
+
+bool cmp2(Student a, Student b) {
+	return a.height > b.height;
+}
+
+int main() {
+	
+	string name;
+	int height;
+	double weight;
+
+	Student students[5];
+
+	for (int i = 0; i < 5; i++) {
+		cin >> name >> height >> weight;
+		students[i] = Student(name, height, weight);
+	}
+	
+	cout << "name" << '\n';
+	sort(students, students + 5, cmp1);
+	for (int i = 0; i < 5; i++) {
+		students[i].print();
+	}
+
+	cout << "height" << '\n';
+	sort(students, students + 5, cmp2);
+	for (int i = 0; i < 5; i++) {
+		students[i].print();
+	}
+
+	return 0;
+}
