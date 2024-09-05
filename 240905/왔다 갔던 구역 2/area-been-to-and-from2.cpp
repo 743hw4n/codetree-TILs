a01e3617f908caa2c522775506f406384dc228a8
ta2hwan <80195853+ta2hwan@users.noreply.github.com>
2024-09-05 17:40:07 +0900

    Title: 왔다 갔던 구역 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240905/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int checked[1001];
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	int location = 500;
+
+	while (n--) {
+		int dis; //거리
+		char dir; //방향
+		cin >> dis >> dir;
+		if (dir == 'R') {
+			while (dis--) {
+				checked[location++]++;
+			}
+		}
+		else {
+			while (dis--) {
+				checked[location--]++;
+			}
+		}
+	}
+
+	int cnt = 0;
+
+	for (int i = 0; i < 1001; i++)
+	{
+		if (checked[i] >= 2)
+			cnt++;
+	}
+	cout << cnt;
+	return 0;
+}
