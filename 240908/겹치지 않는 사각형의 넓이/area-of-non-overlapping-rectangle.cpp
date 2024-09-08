0e383706019f9ec8d42d3734586751d974a7fbb4
ta2hwan <80195853+ta2hwan@users.noreply.github.com>
2024-09-08 12:58:27 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240908/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#define OFFSET 1000
+#define A 1
+#define B 2
+#define M 3
+using namespace std;
+
+int rect[2001][2001];
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+	
+	int x1, y1, x2, y2;
+	cin >> x1 >> y1 >> x2 >> y2;
+
+	for (int i = x1; i < x2; i++)
+	{
+		for (int j = y1; j < y2; j++)
+		{
+			rect[i][j] = A;
+		}
+	}
+
+	cin >> x1 >> y1 >> x2 >> y2;
+
+	for (int i = x1; i < x2; i++)
+	{
+		for (int j = y1; j < y2; j++)
+		{
+			rect[i][j] = B;
+		}
+	}
+
+	cin >> x1 >> y1 >> x2 >> y2;
+
+	for (int i = x1; i < x2; i++)
+	{
+		for (int j = y1; j < y2; j++)
+		{
+			rect[i][j] = M;
+		}
+	}
+
+	int square = 0;
+
+	for (int i = 0; i < 2001; i++) {
+		for (int j = 0; j < 2001; j++)
+		{
+			if (rect[i][j] != M && rect[i][j] != 0) {
+				square++;
+			}
+		}
+	}
+
+	cout << square;
+	return 0;
+}
