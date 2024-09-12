8c559b068b7958dced93620346ac25be83eeac7b
ta2hwan <80195853+ta2hwan@users.noreply.github.com>
2024-09-12 19:08:57 +0900

    Title: 좌우로 움직이는 로봇, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240912/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#define MAX_T 1000000
+using namespace std;
+
+int pos_a[MAX_T + 1], pos_b[MAX_T + 1];
+int n, m;
+
+int main() {
+	cin >> n >> m;
+
+	int time_a = 1;
+	for (int i = 0; i < n; i++)
+	{
+		int t; char d;
+		cin >> t >> d;
+		if (d == 'R') {
+			while (t--) {
+				pos_a[time_a] = pos_a[time_a - 1] + 1;
+				time_a++;
+			}
+		}
+		else {
+			while (t--) {
+				pos_a[time_a] = pos_a[time_a - 1] - 1;
+				time_a++;
+			}
+		}
+	}
+
+	int time_b = 1;
+	for (int i = 0; i < m; i++)
+	{
+		int t; char d;
+		cin >> t >> d;
+		if (d == 'R') {
+			while (t--) {
+				pos_b[time_b] = pos_b[time_b - 1] + 1;
+				time_b++;
+			}
+		}
+		else {
+			while (t--) {
+				pos_b[time_b] = pos_b[time_b - 1] - 1;
+				time_b++;
+			}
+		}
+	}
+
+	int cnt = 0;
+
+	for (int i = 1; i <= max(time_a, time_b); i++)
+	{
+		if (pos_a[i] == pos_b[i]) {
+			if (pos_a[i - 1] != pos_b[i - 1])
+			{
+				//cout << i << endl;
+				cnt++;
+			}
+		}
+	}
+
+	cout << cnt;
+	return 0;
+}
