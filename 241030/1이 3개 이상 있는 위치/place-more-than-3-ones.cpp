37d131d066fa7c9872eb9cf1c8f26eef0fde78b4
ta2hwan <80195853+ta2hwan@users.noreply.github.com>
2024-10-30 21:49:01 +0900

    Title: 1이 3개 이상 있는 위치, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 241030/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
+++ 241030/1이 3개 이상 있는 위치/place-more-than-3-ones.cpp
@@ -8,7 +8,7 @@ int arr[101][101];
 int n;
 
 bool InRange(int x, int y) {
-  return (0 <= x && x < 101 && 0 < 101 && 0 <= y && y < 101);
+  return (0 <= x && x < n && 0 <= y && y < n);
 }
 
 int main() {
@@ -19,25 +19,25 @@ int main() {
       cin >> arr[i][j];
     }
   }
-  int x = 0, y = 0;
 
-  int cntOne = 0;
   int cnt = 0;
 
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
-      cntOne = 0;
+      int cntOne = 0;  // 주변 1의 개수 초기화
       for (int dir_num = 0; dir_num < 4; dir_num++) {
-        int nx = x + dx[dir_num], ny = y + dy[dir_num];
+        int nx = i + dx[dir_num], ny = j + dy[dir_num];
         if (InRange(nx, ny) && arr[nx][ny] == 1)
           cntOne++;
       }
+
+      // 주변 1의 개수가 3 이상이면 cnt 증가
       if (cntOne >= 3)
         cnt++;
     }
   }
 
-  cout << cnt;
+  cout << cnt << endl;
 
   return 0;
 }
