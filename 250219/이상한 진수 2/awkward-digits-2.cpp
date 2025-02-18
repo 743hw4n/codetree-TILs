833fc0d672ce2aa172fdbabb5c2d8a43f326de92
장태환 (TAEHWAN JANG) <80195853+743hw4n@users.noreply.github.com>
2025-02-19 00:36:11 +0900

    Title: 이상한 진수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250219/이상한 진수 2/awkward-digits-2.cpp
+++ 250219/이상한 진수 2/awkward-digits-2.cpp
@@ -3,17 +3,22 @@
 using namespace std;
 
 string a;
-
+bool changed = false;
 int main() {
   cin >> a;
 
   for (int i = 0; i < a.size(); i++) {
     if (a[i] == '0') {
       a[i] = '1';
+      changed = true;
       break;
     }
   }
 
   int max_value = stoi(a, nullptr, 2);
+  if (!changed) {
+    max_value -= 1;
+  }
+
   cout << max_value;
 }
