d017dc4096c7afd48e4eeb7e784de9fdfd3fb91b
장태환 (TAEHWAN JANG) <80195853+743hw4n@users.noreply.github.com>
2025-03-04 22:09:56 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250304/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int grid[100][100];
+
+bool is_happy(int arr[], int length, int m) {
+  int count = 1;
+  for (int i = 1; i < length; i++) {
+    if (arr[i] == arr[i - 1])
+      count++;
+    else
+      count = 1;
+    if (count >= m) return true;
+  }
+  return false;
+}
+int main() {
+  cin >> n >> m;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cin >> grid[i][j];
+    }
+  }
+
+  int happy_count = 0;
+  for (int i = 0; i < n; i++) {
+    if (is_happy(grid[i], n, m)) {
+      happy_count++;
+    }
+  }
+
+  for (int j = 0; j < n; j++) {
+    int temp[100];
+
+    for (int i = 0; i < n; i++) {
+      temp[i] = grid[i][j];
+    }
+    if (is_happy(temp, n, m)) {
+      happy_count++;
+    }
+  }
+
+  cout << happy_count;
+
+  return 0;
+}
